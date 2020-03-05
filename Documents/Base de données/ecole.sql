2d78cc6216b59a7c0d1a0edcaa966a1d5ec8a492
GGEphec <he201732@students.ephec.be>
2020-03-05 01:45:12 +0100

    Base de donnée insert

--- Documents/Base de données/ecole.sql
+++ Documents/Base de données/ecole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  Dim 01 mars 2020 à 16:28
+-- Généré le :  jeu. 05 mars 2020 à 01:43
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `ecole`
 --
-CREATE DATABASE IF NOT EXISTS `ecole` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `ecole`;
 
 -- --------------------------------------------------------
 
@@ -30,15 +28,28 @@ USE `ecole`;
 -- Structure de la table `classes`
 --
 
-DROP TABLE IF EXISTS `classes`;
 CREATE TABLE `classes` (
   `idClasse` int(11) NOT NULL,
   `annee` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONS POUR LA TABLE `classes`:
+-- Déchargement des données de la table `classes`
 --
+
+INSERT INTO `classes` (`idClasse`, `annee`) VALUES
+(1, 'Première A'),
+(2, 'Première B'),
+(3, 'Deuxième A'),
+(4, 'Deuxième B'),
+(5, 'Troisième A'),
+(6, 'Troisième B'),
+(7, 'Quatrième A'),
+(8, 'Quatrième B'),
+(9, 'Cinquième A'),
+(10, 'Cinquième B'),
+(11, 'Sixième A'),
+(12, 'Sixième B');
 
 -- --------------------------------------------------------
 
@@ -46,7 +57,6 @@ CREATE TABLE `classes` (
 -- Structure de la table `eleves`
 --
 
-DROP TABLE IF EXISTS `eleves`;
 CREATE TABLE `eleves` (
   `idEleve` int(11) NOT NULL COMMENT 'L''id de l''élève',
   `nomEleve` varchar(50) NOT NULL COMMENT 'Le nom de l''élève',
@@ -59,14 +69,21 @@ CREATE TABLE `eleves` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONS POUR LA TABLE `eleves`:
---   `classeId`
---       `classes` -> `idClasse`
---   `parent1Id`
---       `parents` -> `idParent`
---   `parent2Id`
---       `parents` -> `idParent`
+-- Déchargement des données de la table `eleves`
 --
+
+INSERT INTO `eleves` (`idEleve`, `nomEleve`, `prenomEleve`, `naissance`, `nationalité`, `classeId`, `parent1Id`, `parent2Id`) VALUES
+(1, 'Colur', 'Pierre', '2011-05-26', 'BELGIUM', 1, 1, 2),
+(2, 'Delporte', 'Aloïs', '2011-03-03', 'BELGIUM', 2, 3, 4),
+(3, 'Grany', 'Talia', '2010-02-08', 'BELGIUM', 3, 5, 6),
+(4, 'Greve', 'Daniel', '2010-08-19', 'BELGIUM', 4, 7, 8),
+(5, 'Greve', 'Tom', '2008-07-15', 'BELGIUM', 5, 7, 8),
+(6, 'Tombor', 'Soraya', '2008-01-02', 'BELGIUM', 6, 9, 10),
+(7, 'Baudin', 'Arthur', '2007-02-22', 'BELGIUM', 7, 11, 12),
+(8, 'Carl', 'Emilie', '2007-05-15', 'BELGIUM', 8, 13, 14),
+(9, 'Latte', 'Ali', '2009-10-10', 'BELGIUM', 9, 15, 16),
+(10, 'Yilmaz', 'Itsvan', '2009-04-17', 'BELGIUM', 10, 17, 18),
+(11, 'Van de Vele', 'Yannis', '2007-12-03', 'BELGIUM', 11, 19, 20);
 
 -- --------------------------------------------------------
 
@@ -74,7 +91,6 @@ CREATE TABLE `eleves` (
 -- Structure de la table `garderie`
 --
 
-DROP TABLE IF EXISTS `garderie`;
 CREATE TABLE `garderie` (
   `idGarderie` int(11) NOT NULL COMMENT 'L''id de la transaction',
   `eleveId` int(11) NOT NULL COMMENT 'L''id de l''élève arrivant/repartant',
@@ -83,19 +99,12 @@ CREATE TABLE `garderie` (
   `in/out` varchar(3) NOT NULL COMMENT 'Si l''elève est arrivé/partit'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- RELATIONS POUR LA TABLE `garderie`:
---   `eleveId`
---       `eleves` -> `idEleve`
---
-
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `parents`
 --
 
-DROP TABLE IF EXISTS `parents`;
 CREATE TABLE `parents` (
   `idParent` int(11) NOT NULL COMMENT 'L''id du parent',
   `nomParent` varchar(50) NOT NULL COMMENT 'Le nom du parent',
@@ -107,8 +116,30 @@ CREATE TABLE `parents` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONS POUR LA TABLE `parents`:
+-- Déchargement des données de la table `parents`
 --
+
+INSERT INTO `parents` (`idParent`, `nomParent`, `prenomParent`, `adresse`, `telephonne`, `GSM`, `email`) VALUES
+(1, 'Colur', 'Michael', 'Rue du port 8 7804 Ath', '068335225', '0478252400', 'm.colur@gmail.com'),
+(2, 'Dekon', 'Patricia', 'Rue du port 8 7804 Ath', '068335225', '0456241230', 'p.dekon@hotmail.fr'),
+(3, 'Delporte', 'Pierre', 'Chaussée de Bruneault 26 7800 Ath', '068245621', '0475262626', 'pierre.delporte@skynet.be'),
+(4, 'Laplace', 'Emilie', 'Chaussée de Bruneault 26 7800 Ath', '068245621', '0489561515', 'emi22laplace@gmail.com'),
+(5, 'Grany', 'Jacques', 'Impasse verte 3 7812 Houtaing', '068549878', '0478141625', 'j.grany@gmail.com'),
+(6, 'Oriban', 'Annie', 'Chaussée de Bruxelles 25 7800 Ath', '068529471', '0478785612', 'annie.oriban@hotmail.com'),
+(7, 'Greve', 'Olivier', 'Route d\'Hacquegnies 10\r\n7911 Frasnes-lez-Anvaing', '069178631', '0478564132', 'o.greve@ibm.fr'),
+(8, 'Ovide', 'Marie', 'Route d\'Hacquegnies 10\r\n7911 Frasnes-lez-Anvaing', '069178631', '0476815462', 'marieovide@gmail.com'),
+(9, 'Tombor', 'Victor', 'Rue du 7 juillet 24 7804 Rebaix', '068794556', '0477451532', 'v.tombor@gmail.com'),
+(10, 'Paul', 'Anna', 'Rue du 7 juillet 24 7804 Rebaix', '068794556', '0477154859', 'a.paul@gmail.com'),
+(11, 'Baudin', 'Michael', 'Chemin des Crolites 47 7800 Ath', '', '0478254948', 'm.baudin@hotmail.fr'),
+(12, 'Druar', 'Pauline', 'Chemin des Crolites 47 7800 Ath', '', '0475124589', 'p.druar@gmail.com'),
+(13, 'Carl', 'Sacha', 'Rue du Trieu Périlleux 29 7810 Ath', '068741245', '0478154923', 's.carl@hotmail.com'),
+(14, 'Party', 'Zélie', 'Rue du Trieu Périlleux 29 7810 Ath', '068741245', '0476321459', 'zelie.party@skynet.be'),
+(15, 'Latte', 'Kevin', 'Clos des Comtes du Hainaut 2 7800 Ath', '068179354', '0476324875', 'k.latte@gmail.com'),
+(16, 'Hubert', 'Camille', 'Clos des Comtes du Hainaut 2 7800 Ath', '068179354', '0478149765', 'camille.hubert@gmail.com'),
+(17, 'Yilmaz', 'Maxence', 'Place de Villers-Notre-Dame 20 7812 Ath', '068473214', '0471598746', 'm.yilmaz@gmail.ru'),
+(18, 'Perez', 'Katty', 'Place de Villers-Notre-Dame 20 7812 Ath', '068473214', '0471587648', 'kattyperez@hotmail.fr'),
+(19, 'Van de Vele', 'Dorian', 'Rue de Moulbaix 4 7903 Leuze-en-Hainaut', '068714987', '0472654897', 'd.vandevele@hotmail.fr'),
+(20, 'Slager', 'Christine', 'Rue de Moulbaix 4 7903 Leuze-en-Hainaut', '068714987', '0478214598', 'c.slager@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -116,7 +147,6 @@ CREATE TABLE `parents` (
 -- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
 CREATE TABLE `utilisateurs` (
   `idUtilisateur` int(11) NOT NULL COMMENT 'L''id de l''utilisateur',
   `login` varchar(50) NOT NULL COMMENT 'Le login',
@@ -128,8 +158,15 @@ CREATE TABLE `utilisateurs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- RELATIONS POUR LA TABLE `utilisateurs`:
+-- Déchargement des données de la table `utilisateurs`
 --
+
+INSERT INTO `utilisateurs` (`idUtilisateur`, `login`, `motDePasse`, `nomUtilisateur`, `prenomUtilisateur`, `role`, `droits`) VALUES
+(0, 'root', 'toor', 'root', 'root', 'Administrateur', 0),
+(1, 'dir01', 'dir01', 'Leleux', 'Sylvie', 'Directrice', 1),
+(2, 'secretariat01', 'secretariat', 'Mandosa', 'Lucas', 'Secrétaire', 2),
+(3, 'secretariat02', 'secretariat', 'Mendez', 'Charlotte', 'Secrétaire', 2),
+(4, 'garderie01', 'garderie', 'Perez', 'Martine', 'Garderie', 3);
 
 --
 -- Index pour les tables déchargées

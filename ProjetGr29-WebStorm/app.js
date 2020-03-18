20fa4bd41af2edd42ac53ac633f81edd96954037
Gaelol <37298427+Gaelol@users.noreply.github.com>
2020-03-18 11:41:51 +0100

    ajout de test

--- ProjetGr29-WebStorm/app.js
+++ ProjetGr29-WebStorm/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
+var testRouter = require('./routes/test');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/users', usersRouter);
+app.use('/users', testRouter);
 
 
 // catch 404 and forward to error handler

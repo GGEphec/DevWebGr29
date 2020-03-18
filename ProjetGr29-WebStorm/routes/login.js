67240672a5891cbd4ddad35e49117085842d63e0
Gaelol <37298427+Gaelol@users.noreply.github.com>
2020-03-18 15:01:54 +0100

    login 3rd push

--- ProjetGr29-WebStorm/routes/login.js
+++ ProjetGr29-WebStorm/routes/login.js
@@ -1,58 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-
-
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'ecole'
-});
-var app = express();
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('login');
 });
-/*
-app.use('/users', function(request, response) {
-    var username = request.body.username;
-    var password = request.body.password;
-    if (username && password) {
-        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-            if (results.length > 0) {
-                request.session.loggedin = true;
-                request.session.username = username;
-                response.redirect('/users');
-            } else {
-                response.send('Incorrect Username and/or Password!');
-            }
-            response.end();
-        });
-    } else {
-        response.send('Please enter Username and Password!');
-        response.end();
-    }
-});
 
-app.get('/users', function(request, response) {
-    if (request.session.loggedin) {
-        response.send('Welcome back, ' + request.session.username + '!');
-        console.log('salut');
-    } else {
-        response.send('Please login to view this page!');
-    }
-    response.end();
-});
-*/
 module.exports = router;
--- ProjetGr29-WebStorm/routes/users.js
+++ ProjetGr29-WebStorm/routes/users.js
@@ -1,10 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.render('users', {title : 'Users'});
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'ecole'
 });
+
+var app = express();
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+router.get('/', function(req, res, next) {
+  var page = url.parse(req.url).pathname;
+  var param = querystring.parse(url.parse(req.url).query);
+
+ if(param.username == 'root') {
+   res.render('users', {title: 'Users', nom : param.username});
+ }
+});
+
+/*app.post('/auth', function(request, response) {
+  var username = param.username;
+  var password = param.password;
+  if (username && password) {
+    connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+      if (results.length > 0) {
+        request.session.loggedin = true;
+        request.session.username = username;
+      } else {
+        response.send('Incorrect Username and/or Password!');
+      }
+      response.end();
+    });
+  }
+});*/
 
 module.exports = router;

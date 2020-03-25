e8f588f9091cc33e7762560f1dfe9932eab92c19
GGEphec <he201732@students.ephec.be>
2020-03-25 10:57:18 +0100

    api parents

--- ProjetGr29-WebStorm/routes/users.js
+++ ProjetGr29-WebStorm/routes/users.js
@@ -4,11 +4,28 @@ var app = express();
 
 
 
-router.get('/', function(req, res, next) {
+router.get('/eleves', function(req, res, next) {
     res.locals.connection.query('SELECT * from eleves', function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
 });
+
+router.get('/parents', function (req, res, next) {
+    res.locals.connection.query('SELECT * FROM parents', function (error, results, fields) {
+        if(error) throw error;
+        res.send(JSON.stringify(({"status": 200, "error":null, "reponse":results})))
+    });
+    
+});
+
+router.post('/eleve', function (req, res, next) {
+   res.locals.connection.query(,function(error, results, fields){
+       if(error) throw error;
+
+   });
+});
+
+
 
 module.exports = router;

917ad5920f026a4f324e3e622fcd56434d4f6f5b
NathDL <he201742@students.ephec.be>
2020-03-25 14:08:24 +0100

    UPDATE Master Propre

--- ProjetGr29-WebStorm/routes/api.js
+++ ProjetGr29-WebStorm/routes/api.js
@@ -35,17 +35,7 @@ router.get('/eleves', function(req, res, next) {
             res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
         })};
 });
-/*router.get('/eleves/id', function(req, res, next) {
-    var user_id = url.parse(req.url).pathname;
-    console.log(user_id);
-    if (typeof user_id != null) {
-        res.locals.connection.query('SELECT * FROM eleves WHERE idEleve = ?{user_id}', function (error, results, fields) {
-            if (error) throw error;
-            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        });
-    }
-})
-*/
+
 router.get('/parents', function (req, res, next) {
     res.locals.connection.query('SELECT * FROM parents', function (error, results, fields) {
         if(error) throw error;

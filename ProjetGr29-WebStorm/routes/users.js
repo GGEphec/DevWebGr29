0c28ffeb0d04b8b348a10a3534dae1da42f33101
NathDL <he201742@students.ephec.be>
2020-03-25 11:01:12 +0100

    API User + Params

--- ProjetGr29-WebStorm/routes/users.js
+++ ProjetGr29-WebStorm/routes/users.js
@@ -4,11 +4,23 @@ var app = express();
 
 
 
-router.get('/', function(req, res, next) {
-    res.locals.connection.query('SELECT * from eleves', function (error, results, fields) {
-        if (error) throw error;
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-    });
+router.get('/eleves', function(req, res, next) {
+    var user_id = req.param('id');
+    var user_name = req.param('name');
+    if (typeof user_id != null) {
+        res.locals.connection.query('SELECT * FROM eleves WHERE idEleve = ${user_id}', function (error, results, fields) {
+            if (error) throw error;
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        });
+    }
+    else {
+        res.locals.connection.query('SELECT * from eleves', function (error, results, fields) {
+            if (error) throw error;
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        });
+    }
 });
+
+
 
 module.exports = router;

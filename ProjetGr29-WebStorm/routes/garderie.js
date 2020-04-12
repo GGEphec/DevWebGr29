42cacb90a7dfc0c5d75dadb1cf280a84b4080657
NathDL <he201742@students.ephec.be>
2020-04-12 15:56:11 +0200

    Update HEAD supprimés

--- ProjetGr29-WebStorm/routes/garderie.js
+++ ProjetGr29-WebStorm/routes/garderie.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-const request = require('request');
-
-// Affichage de la page HTML
-router.get('/', function(req, res) {
-    res.render('garderie');
-});
-
-module.exports = router;
-=======
-
 var express = require('express');
 var router = express.Router();
 var app = express();
@@ -33,4 +20,4 @@ router.get('/', function(req, res){
 
 
 module.exports = router;
->>>>>>> 80e12b9938bce6d6ce3e3e6499825f13914a695a
+

b04a1d6c7322780e105022da73ded993323303e3
GGEphec <he201732@students.ephec.be>
2020-04-17 15:17:17 +0200

    login ok

--- ProjetGr29-WebStorm/routes/garderie.js
+++ ProjetGr29-WebStorm/routes/garderie.js
@@ -28,32 +28,6 @@ router.get('/', function(req, res){
    res.render('garderie', {garderieTableau:gard});
 });
 
-/*//Ajoute des lignes dans le tableau de la garderie
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '',
-   database : 'ecole'
-});
-
-var idGarderie = document.getElementById("idGarderie").value;
-console.log(idGarderie);
-var idEleve = document.getElementById("idEleve").value;
-var date = document.getElementById("date").value;
-var heure = document.getElementById("heure").value;
-var outin = document.getElementById("outin").value;
-*/
-con.connect(function(err) {
-   if (err) throw err;
-   console.log("Connected!");
-   //var sql = "INSERT INTO garderie (idGarderie, idEleve, date, heure, outin) VALUES (100, 10, '2020-04-01', '08h10', 'in')";
-   con.query("INSERT INTO garderie (idGarderie, idEleve, date, heure, outin) VALUES (?, ?, ?, ?, ?)", [ idGarderie, idEleve, date, heure, outin], function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
-   });
-});
 
 
 module.exports = router;
--- ProjetGr29-WebStorm/routes/usersold.js
+++ ProjetGr29-WebStorm/routes/usersold.js
@@ -1,43 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var url = require('url');
 var querystring = require('querystring');
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var page = url.parse(req.url).pathname;
+router.get('/', function(req, res) {
     var param = querystring.parse(url.parse(req.url).query);
     var username = param.username;
     var password = param.password;
-    if (username && password) {
-        res.locals.connection.query('SELECT * FROM utilisateurs WHERE login = ? and motDePasse = ?', [username,password], function(error, results, fields) {
-            if (results.length> 0) { //On redirige vers la bonne page si le mdp est correct
-                switch (username){
-                    case 'secretariat01' :
-                        res.redirect('/secretariat');
-                        break;
-                    case 'secretariat02' :
-                        res.redirect('/secretariat');
-                        break;
-                    case 'dir01' :
-                        res.redirect('/secretariat');
-                        break;
-                    case 'garderie01':
-                        res.redirect('/garderie');
-                        break;
-                    case 'garderie02':
-                        res.redirect('/garderie');
-                        break;
-                    default:
-                        res.send('Utilisateur inconnu')
-                }
-            }
-            else {
-                res.send('Incorrect Username and/or Password!');
-            }
-            res.end();
-        });
 
-    }
+    const option = {
+        url: 'http://localhost:3000/api/v1/login?username='+username+'&password='+password,
+        method: 'GET'
+    };
+    request(option, function (err, res2, results) {
+        var json = JSON.parse(results)['response'];
+        console.log(json[0]['droits']);
+        if (json.length > 0) { //On redirige vers la bonne page si le mdp est correct
+            switch (json[0]['droits']) {
+                case 1 :
+                    res.redirect('/secretariat');
+                    break;
+                case 2 :
+                    res.redirect('/secretariat');
+                    break;
+                case 3:
+                    res.redirect('/garderie');
+                    break;
+            }
+        } else {
+            res2.send('Incorrect Username and/or Password!');
+        }
+        res.render('login');
+    });
 });
+
 
 module.exports = router;

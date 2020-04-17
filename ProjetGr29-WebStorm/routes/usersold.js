8c80ce6190adc1ac1d994d7bbf68a422d11b3d4e
GGEphec <he201732@students.ephec.be>
2020-04-17 16:42:09 +0200

    merge reglé

--- ProjetGr29-WebStorm/routes/usersold.js
+++ ProjetGr29-WebStorm/routes/usersold.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var url = require('url');
+var request = require('request');
 var querystring = require('querystring');
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,26 +9,6 @@ router.get('/', function(req, res, next) {
     var param = querystring.parse(url.parse(req.url).query);
     var username = param.username;
     var password = param.password;
-    if (username && password) {
-        res.locals.connection.query('SELECT * FROM utilisateurs WHERE login = ? and motDePasse = ?', [username,password], function(error, results, fields) {
-            if (results.length> 0) { //On redirige vers la bonne page si le mdp est correct
-                switch (results[0].droits){
-                    case '1' :
-                        res.redirect('/secretariat');
-                        break;
-                    case '2' :
-                        res.redirect('/secretariat');
-                        break;
-                    case '3':
-                        res.redirect('/garderie');
-                        break;
-                }
-            }
-            else {
-                res.send('Incorrect Username and/or Password!');
-            }
-            res.end();
-        });
 
     const option = {
         url: 'http://localhost:3000/api/v1/login?username='+username+'&password='+password,

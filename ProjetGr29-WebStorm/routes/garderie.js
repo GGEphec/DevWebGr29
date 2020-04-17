a57c2884bc0d45393c96b7e60eec1af20c450757
NathDL <he201742@students.ephec.be>
2020-04-17 13:52:51 +0200

    Garderie Update - Insert new line

--- ProjetGr29-WebStorm/routes/garderie.js
+++ ProjetGr29-WebStorm/routes/garderie.js
@@ -7,6 +7,7 @@ const options = {
    method : 'GET'
 }
 
+// Affichage le tableau de la garderie
 var gard=[];
 request(options, function(err, res2, data) {
    var json = JSON.parse(data)['response'];
@@ -23,11 +24,36 @@ request(options, function(err, res2, data) {
    }
 });
 
-
 router.get('/', function(req, res){
    res.render('garderie', {garderieTableau:gard});
 });
 
+/*//Ajoute des lignes dans le tableau de la garderie
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '',
+   database : 'ecole'
+});
+
+var idGarderie = document.getElementById("idGarderie").value;
+console.log(idGarderie);
+var idEleve = document.getElementById("idEleve").value;
+var date = document.getElementById("date").value;
+var heure = document.getElementById("heure").value;
+var outin = document.getElementById("outin").value;
+*/
+con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+   //var sql = "INSERT INTO garderie (idGarderie, idEleve, date, heure, outin) VALUES (100, 10, '2020-04-01', '08h10', 'in')";
+   con.query("INSERT INTO garderie (idGarderie, idEleve, date, heure, outin) VALUES (?, ?, ?, ?, ?)", [ idGarderie, idEleve, date, heure, outin], function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+   });
+});
 
 
 module.exports = router;
